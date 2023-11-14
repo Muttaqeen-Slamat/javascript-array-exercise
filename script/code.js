b0a5b43f69b895fcd061b9074e769f5dbcce5a85
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-11-14 16:59:55 +0200

    getting solved

--- script/code.js
+++ script/code.js
@@ -1,8 +1,8 @@
-let number = [12, 18, 11, 5, 8, 2];
-//showing the array length
-console.log(numbers.length);
-//displaying lenght half
-console.log(numbers.length/2)
+// let number = [12, 18, 11, 5, 8, 2];
+// //showing the array length
+// console.log(numbers.length);
+// //displaying lenght half
+// console.log(numbers.length/2)
 
 
 
@@ -25,17 +25,14 @@ console.log(numbers.length/2)
 function displayMiddleNumber(numbers) {
     const length = numbers.length;
     if (length % 2 === 0) {
-      // If the array has an even number of elements, return the two middle numbers
       const middleIndex1 = length / 2 - 1;
       const middleIndex2 = length / 2;
       console.log(`Two middle numbers: ${numbers[middleIndex1]}, ${numbers[middleIndex2]}`);
     } else {
-      // If the array has an odd number of elements, return the middle number
       const middleIndex = Math.floor(length / 2);
       console.log(`Middle number: ${numbers[middleIndex]}`);
     }
   }
-  // Example usage with the provided array
   let numbers = [12, 18, 11, 5, 8, 2];
   displayMiddleNumber(numbers);
 
