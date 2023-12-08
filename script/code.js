d5d857749920b8225d5057549ba7b5149da6cf63
Muttaqeen Slamat <muttaqeens@gmail.com>
2023-12-08 06:27:15 +0200

    fix

--- script/code.js
+++ script/code.js
@@ -159,3 +159,71 @@ function addProduct() {
     console.error('Error in addProduct:', e.message);
   }
 }
+
+
+
+
+
+// js checkout
+
+document.querySelector('#year').textContent = new Date().getFullYear();
+
+let checkout = JSON.parse(localStorage.getItem('checkout')) || [];
+
+let checkoutWrapper = document.querySelector('[featured-checkout]');
+let space = document.querySelector('[space]');
+
+// Define a simple groupBy function
+Object.groupBy = function (array, keyGetter) {
+    const map = new Map();
+    array.forEach((item) => {
+        const key = keyGetter(item);
+        const collection = map.get(key);
+        if (!collection) {
+            map.set(key, [item]);
+        } else {
+            collection.push(item);
+        }
+    });
+    return map;
+};
+
+let group = Object.groupBy(checkout, (item) => item.name);
+
+function displayCheckout() {
+    checkoutWrapper.innerHTML = '';
+    try {
+        if (checkout.length > 0) {
+            for (const [name, items] of group.entries()) {
+                checkoutWrapper.innerHTML += `
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>${name}</td>
+                    <td><input class="quantity-input" type="number" value="${items.length}" min="1"></td>
+                    <td>R${items[0].price * items.length}</td>
+                  </tr>
+                  `;
+            }
+        } else {
+            space.innerHTML = `<div class="text-center">No Items In Cart <i class="bi bi-emoji-frown"></i></div>`;
+        }
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+function clearBtn() {
+    if (checkout.length > 0) {
+        checkout.splice(0, checkout.length);
+        displayCheckout();
+        clearedCheckout();
+    }
+}
+
+function clearedCheckout() {
+    localStorage.setItem('checkout', JSON.stringify(checkout));
+}
+
+document.querySelector('[clear-table]').addEventListener('click', clearBtn);
+
+displayCheckout();
