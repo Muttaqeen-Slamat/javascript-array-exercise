f6bdf3d252b06065d56b8b6d2151c9bbc906cc06
Muttaqeen Slamat <muttaqeens@gmail.com>
2023-12-08 06:02:17 +0200

    fix

--- script/code.js
+++ script/code.js
@@ -57,3 +57,105 @@ function deleteProduct(){
 deleteProduct()
 
 del.addEventListener('click', deleteProduct)
+
+document.querySelector('#year').textContent = new Date().getFullYear();
+
+let admin = JSON.parse(localStorage.getItem('products')) || [];
+let adminTable = document.querySelector('[admin-staff]');
+
+function adminContent() {
+  try {
+    let products = JSON.parse(localStorage.getItem('products')) || [];
+    adminTable.innerHTML = '';
+
+    products.forEach((product, i) => {
+      adminTable.innerHTML += `
+        <tr class="text-center">
+          <td>${product.name}</td>
+          <td><img src="${product.image}" id="adminImg"></td>
+          <td>
+            <button id="adminEdit" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${i}">
+              <i class="bi bi-magic"></i>
+            </button>
+          </td>
+          <td>
+            <button id="adminDelete" admin-delete onclick='deleteProduct(${i})'>
+              <i class="bi bi-trash3"></i>
+            </button>
+          </td>
+        </tr>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal${i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">${product.name}</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <label for="recipient-name-${i}" class="col-form-label">Name:</label>
+                <input type="text" class="form-control" id="recipient-name-${i}" value="${product.name}">
+              </div>
+              <div class="modal-body">
+                <label for="recipient-spec-${i}" class="col-form-label">Specs:</label>
+                <input type="text" class="form-control" id="recipient-spec-${i}" value="${product.spec}">
+              </div>
+              <div class="modal-body">
+                <label for="recipient-price-${i}" class="col-form-label">Price:</label>
+                <input type="text" class="form-control" id="recipient-price-${i}" value="${product.price}">
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" onclick='updateProduct(${i})'>Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>`;
+    });
+  } catch (e) {
+    console.error('Error in adminContent:', e.message);
+  }
+}
+
+adminContent();
+
+let addBtn = document.querySelector('[admin-add]');
+let addModal = document.querySelector('#addModal');
+let addNameInput = document.querySelector('#addName');
+let addSpecInput = document.querySelector('#addSpec');
+let addPriceInput = document.querySelector('#addPrice');
+
+addBtn.addEventListener('click', function () {
+  addModal.show();
+});
+
+function addProduct() {
+  try {
+    let newName = addNameInput.value;
+    let newSpec = addSpecInput.value;
+    let newPrice = addPriceInput.value;
+
+    if (!newName || !newSpec || !newPrice) {
+      // Check if any field is empty
+      alert('Please fill in all fields');
+      return;
+    }
+
+    let newProduct = {
+      name: newName,
+      spec: newSpec,
+      price: newPrice,
+      image: 'path/to/default/image.jpg', // Set a default image path
+    };
+
+    let products = JSON.parse(localStorage.getItem('products')) || [];
+    products.push(newProduct);
+    localStorage.setItem('products', JSON.stringify(products));
+
+    adminContent();
+    addModal.hide();
+  } catch (e) {
+    console.error('Error in addProduct:', e.message);
+  }
+}
